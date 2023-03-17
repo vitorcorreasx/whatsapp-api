5069a665a8596a2811eba6455648db456294d55a
Juliano Stroschon <julianostroschon@gmail.com>
2023-03-17 15:40:25 -0300

    🔒️ permitir somente POST e para a rota padrão

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const { send } = require('./src/services/whatsapp');
 const HOST = process.env.HOST || 'http://localhost';
 const SECRET = process.env.PORT || 'secret';
 const PORT = process.env.PORT || 3001;
+const BASE_ROUTE = '/api/v1/send?token=';
 
 async function sendResponse(res, error, decodedToken) {
   res.writeHead(200, { 'Content-Type': 'application/json' });
@@ -25,8 +26,12 @@ async function sendResponse(res, error, decodedToken) {
 
 const server = http.createServer((req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
-
   const token = url.searchParams.get('token');
+
+  if (req.method !== 'POST' || !req.url.startsWith(BASE_ROUTE)) {
+    res.statusCode = 405; // Status code for "Method Not Allowed"
+    return res.end('Only POST requests are allowed');
+  }
 
   verify(token, SECRET, { complete: true }, (error, decoded) =>
     sendResponse(res, error, decoded)
