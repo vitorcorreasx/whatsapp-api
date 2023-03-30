f11e2e5f3861a4087e19b7e35add92e433afe167
Juliano Stroschon <julianostroschon@gmail.com>
2023-03-30 13:46:08 -0300

    ♻️ alterado arquivo do server

--- src/server/index.js
+++ src/server/index.js
@@ -1,53 +1,50 @@
 const { verify } = require('jsonwebtoken');
 const http = require('http');
 
-const { constructClient } = require('../services/whatsapp');
-const { send } = require('../services/whatsapp');
+const {
+  ALLOWED_METHOD,
+  HOST,
+  BASE_ROUTE,
+  SECRET,
+  senResponse,
+} = require('../util');
+const { buildClient, send } = require('../services/whatsapp');
 
-const BASE_ROUTE = '/api/v1/send?token=';
-const SECRET = process.env.SECRET || 'segredo';
-const HOST = process.env.HOST || 'http://localhost';
+async function buildServer(clientId = 'client-one') {
+  const client = await buildClient(clientId);
 
-async function sendResponse(res, error, decodedToken, client) {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-
-  if (error) {
-    return res.end(
-      JSON.stringify({ statusMessage: false, error: 'Invalid token' })
-    );
-  }
-  console.log('Token valid');
-
-  const { phonenumber, message } = decodedToken.payload;
-
-  const response = await send(phonenumber, message, client);
-
-  return res.end(JSON.stringify(response));
-}
-
-async function buildServer() {
-  const { client, messages } = await constructClient('client-one');
-  // console.log(service);
   await client.initialize();
-  console.log('client running');
-  // await service.client.sendMessage(`${5555996929042}@c.us`, 'Hello');
-  const app = http.createServer((req, res) => {
-    const url = new URL(req.url, `http://${req.headers.host}`);
-    const token = url.searchParams.get('token');
 
-    if (req.method !== 'POST' || !req.url.startsWith(BASE_ROUTE)) {
-      res.statusCode = 405; // Status code for "Method Not Allowed"
+  const app = http.createServer((req, res) => {
+    if (req.method !== ALLOWED_METHOD || !req.url.startsWith(BASE_ROUTE)) {
+      res.statusCode = 405;
       return res.end('Only POST requests are allowed');
     }
 
-    verify(token, SECRET, { complete: true }, (error, decoded) => {
-      sendResponse(res, error, decoded, client);
+    const { searchParams } = new URL(req.url, `http://${req.headers.host}`);
+
+    const token = searchParams.get('token');
+
+    verify(token, SECRET, { complete: true }, async (error, { payload }) => {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+
+      if (error) {
+        return senResponse(res, {
+          statusMessage: false,
+          error: 'Invalid token',
+        });
+      }
+      const { phonenumber, message } = payload;
+
+      const response = await send(phonenumber, message, client);
+
+      return senResponse(res, response);
     });
   });
 
   return {
     /**
-     *
+     * Define qual porta o servidor vai receber requisições
      * @param {number} port
      */
     listen: (port) => {
