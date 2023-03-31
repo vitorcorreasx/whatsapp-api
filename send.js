1126f33d41838ea01e86550b976bbcdfcc8f0129
Juliano Stroschon <julianostroschon@gmail.com>
2023-03-31 16:32:31 -0300

    🚑️ update send

--- send.js
+++ send.js
@@ -14,11 +14,16 @@ function constructToken(message = 'Hello World', phonenumber = 5555999999999) {
   return sign({ message, phonenumber }, SECRET);
 }
 
+const token = constructToken();
+
 const options = {
-  path: BASE_ROUTE + constructToken(),
+  path: BASE_ROUTE,
   method: ALLOWED_METHOD,
   hostname: HOST.replace('http://', ''),
   port: PORT,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 };
 
 const req = http.request(options, (res) => {
@@ -28,6 +33,7 @@ const req = http.request(options, (res) => {
     return process.stdout.write(d);
   });
 });
+req.write(JSON.stringify({ token }));
 
 req.on('error', (error) => {
   console.error(error);
